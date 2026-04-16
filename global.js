65c06aff804e8fec58f36f35c839065b740a0df7
Kyle Dang <127087021+Kyle-dang678@users.noreply.github.com>
2026-04-15 17:21:17 -0700

    Update contact page and global.js

--- global.js
+++ global.js
@@ -71,3 +71,14 @@ select.addEventListener('input', function(event) {
     document.documentElement.style.setProperty('colorScheme', event.target.value);
     localStorage.colorScheme = event.target.value;
 });
+
+let form = document.querySelector('form');
+form?.addEventListener('submit', function(event){
+    event.preventDefault();
+    let data = new FormData(form);
+    let url = form.action + '?';
+    for (let[name, value] of data) {
+        url += `${name}=${encodeURIComponent(value)}&`;
+    }
+    location.href = url;
+});
