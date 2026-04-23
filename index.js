6c54c0511fbf733ac8a869ee68a26ba05e8e7617
Kyle Dang <127087021+Kyle-dang678@users.noreply.github.com>
2026-04-22 18:50:34 -0700

    Update homepage html js and global js

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ import { fetchJSON, renderProjects, fetchGithubData } from "./global.js";
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0,3);
 const projectsContainer = document.querySelector('.projects');
+const githubData = await fetchGithubData('giorgiancolaou');
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+    profileStats.innerHTML = `
+        <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+        </dl>
+    `;
+}
 renderProjects(latestProjects, projectsContainer, 'h2');
+
