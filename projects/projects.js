a58d6647ae7a4cd3319e74e5331ff80b5d5d9f83
Kyle Dang <127087021+Kyle-dang678@users.noreply.github.com>
2026-04-29 16:21:22 -0700

    Update project.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,6 @@
 import { fetchJSON, renderProjects } from "../global.js";
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 const projectsTitle = document.querySelector('.projects-title');
@@ -7,9 +8,15 @@ projectsTitle.textContent = `Projects (${projects.length})`;
 renderProjects(projects, projectsContainer, 'h2');
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-    startAngle: 0,
-    endAngle: 2 * Math.PI,
-});
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+let data = [1, 2];
+let sliceGenerator = d3.pie()
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+arcs.forEach((arc) => {
+    d3.select('svg')
+        .append('path')
+        .attr('d', arc)
+        .attr('fill', colors(idx));
+});
