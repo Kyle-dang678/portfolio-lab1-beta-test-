619a2221a78ffece952790e7690e1b138edcc70c
Kyle Dang <127087021+Kyle-dang678@users.noreply.github.com>
2026-04-29 15:22:41 -0700

    Update project js and index

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from "../global.js";
+import * as df from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 const projectsTitle = document.querySelector('.projects-title');
